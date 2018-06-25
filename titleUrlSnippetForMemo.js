97b324aeb2ad9c4266f4add4f1a810519fc200e3
Yusuke Konishi <ysk.kns@gmail.com>
2018-06-25 11:39:24 +0900

    Add bookmarklet for getting current page title, url and snippet for memo

---
+++ titleUrlSnippetForMemo.js
@@ -0,0 +1,8 @@
+  javascript: (() => {
+    const titleUrlSnippet = document.title + '\n'
+      + location.href + '\n'
+      + '---\n'
+      + window.getSelection().toString() + '\n'
+      + '---\n';
+    prompt(titleUrlSnippet, titleUrlSnippet);
+  })();
