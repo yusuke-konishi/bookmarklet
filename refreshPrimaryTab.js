cb94d1f4ac39ef4f0094a01ad96694213c70b5e5
Yusuke Konishi <ysk.kns@gmail.com>
2018-10-07 11:24:43 +0900

    refactor comment message again...

--- refreshPrimaryTab.js
+++ refreshPrimaryTab.js
@@ -13,7 +13,7 @@ javascript: (() => {
     script.type = 'text/javascript';
     document.head.appendChild(script);
 
-    /* callback function executed after the <script> element is loaded */
+    /* callback function executed when the <script> element is loaded */
     script.onload = () => {
         let primaryTabId;
 
