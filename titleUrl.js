9939cd7ecaee65a69f5080a2bc7e5a12ffcab90a
Yusuke Konishi <ysk.kns@gmail.com>
2018-10-07 22:55:48 +0900

    change indent from 2 spaces to 4

--- titleUrl.js
+++ titleUrl.js
@@ -1,4 +1,4 @@
 javascript: (() => {
-  const titleUrl = document.title + '\n' + location.href;
-  prompt(titleUrl, titleUrl);
+    const titleUrl = document.title + '\n' + location.href;
+    prompt(titleUrl, titleUrl);
 })();
--- titleUrlSnippet.js
+++ titleUrlSnippet.js
@@ -1,11 +1,11 @@
 javascript: (() => {
-  const titleUrlSnippet = '<ご参考情報>\n'
-    + document.title + '\n'
-    + location.href + '\n'
-    + '\n'
-    + '<抜粋>\n'
-    + '---\n'
-    + window.getSelection().toString() + '\n'
-    + '---\n';
-  prompt(titleUrlSnippet, titleUrlSnippet);
+    const titleUrlSnippet = '<ご参考情報>\n'
+        + document.title + '\n'
+        + location.href + '\n'
+        + '\n'
+        + '<抜粋>\n'
+        + '---\n'
+        + window.getSelection().toString() + '\n'
+        + '---\n';
+    prompt(titleUrlSnippet, titleUrlSnippet);
 })();
