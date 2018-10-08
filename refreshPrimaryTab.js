1db91ad2ca1f2fe807b2825e88fa3cfb54c4aa9f
Yusuke Konishi <ysk.kns@gmail.com>
2018-10-08 11:31:34 +0900

    define function to load external JavaScript file and set callback for onload

--- refreshPrimaryTab.js
+++ refreshPrimaryTab.js
@@ -1,22 +1,28 @@
 javascript: (() => {
-    /* alert and exit if this program is executed out of ".salesforce.com/console" */
+    /* alert and exit if this program is executed out of "*.salesforce.com/console" */
     if (!/\.salesforce\.com\/console/.test(location.href)) {
-        alert(`This program must be executed in ".salesforce.com/console".\n\ncurrent url: ${location.href}`);
+        alert(`This program must be executed in "*.salesforce.com/console".\n\ncurrent url: ${location.href}`);
         return;
     }
     console.log(`current url: ${location.href}`);
 
-    /* create <script> element and load Salesforce Console Integration Toolkit */
+    /* define function to load external JavaScript file and set callback for onload */
+    function loadJs(jsUrl, callback) {
+        let script = document.createElement('script');
+        script.src = jsUrl;
+        script.type = 'text/javascript';
+        document.head.appendChild(script);
+
+        script.onload = () => {
+            callback();
+        };
+    };
+
+    let primaryTabId;
+
+    /* load Salesforce Console Integration Toolkit */
     /* https://developer.salesforce.com/docs/atlas.en-us.214.0.api_console.meta/api_console/sforce_api_console_connecting.htm */
-    let script = document.createElement('script');
-    script.src = '/support/console/43.0/integration.js';
-    script.type = 'text/javascript';
-    document.head.appendChild(script);
-
-    /* callback function executed when the <script> element is loaded */
-    script.onload = () => {
-        let primaryTabId;
-
+    loadJs('/support/console/43.0/integration.js', () => {
         /* get primary tab id by getFocusedPrimaryTabId() */
         /* https://developer.salesforce.com/docs/atlas.en-us.214.0.api_console.meta/api_console/sforce_api_console_getfocusedprimarytabid.htm */
         sforce.console.getFocusedPrimaryTabId((result) => {
@@ -33,5 +39,5 @@ javascript: (() => {
                 alert(`Primary tab was not refreshed successfully.`);
             }
         });
-    };
+    });
 })();
