502da72c57a8ece532963d95a22bdaca049fb5dd
Yusuke Konishi <ysk.kns@gmail.com>
2018-10-08 13:46:21 +0900

    change order to reduce processing if the primary tab object is not Case

--- caseNumberSubjectUrl.js
+++ caseNumberSubjectUrl.js
@@ -18,19 +18,12 @@ javascript: (() => {
         };
     };
 
-    let primaryTabId;
     let primaryTabObjectId;
+    let primaryTabId;
 
     /* load Salesforce Console Integration Toolkit */
     /* https://developer.salesforce.com/docs/atlas.en-us.214.0.api_console.meta/api_console/sforce_api_console_connecting.htm */
     loadJs('/support/console/43.0/integration.js', () => {
-        /* get primary tab id by getFocusedPrimaryTabId() */
-        /* https://developer.salesforce.com/docs/atlas.en-us.214.0.api_console.meta/api_console/sforce_api_console_getfocusedprimarytabid.htm */
-        sforce.console.getFocusedPrimaryTabId((result) => {
-            primaryTabId = result.id;
-        });
-        console.log(`primary tab id: ${primaryTabId}`);
-
         /* get primary tab object id by getFocusedPrimaryTabObjectId() */
         /* https://developer.salesforce.com/docs/atlas.en-us.214.0.api_console.meta/api_console/sforce_api_console_getfocusedprimarytabobjectid.htm */
         sforce.console.getFocusedPrimaryTabObjectId((result) => {
@@ -44,5 +37,12 @@ javascript: (() => {
             alert(`This program can be executed when the primary tab is showing Case object (Case object id is 500*).\n\nprimary tab object id: ${primaryTabObjectId}`);
             return;
         }
+
+        /* get primary tab id by getFocusedPrimaryTabId() */
+        /* https://developer.salesforce.com/docs/atlas.en-us.214.0.api_console.meta/api_console/sforce_api_console_getfocusedprimarytabid.htm */
+        sforce.console.getFocusedPrimaryTabId((result) => {
+            primaryTabId = result.id;
+        });
+        console.log(`primary tab id: ${primaryTabId}`);
     });
 })();
