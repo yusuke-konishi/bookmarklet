1a72aff682d783ab58c7e502410ac2cb12334864
Yusuke Konishi <ysk.kns@gmail.com>
2018-10-15 18:54:48 +0200

    update comment/alert messages and indent for the public version

--- prototype/testUrl.js
+++ prototype/testUrl.js
@@ -1,9 +1,9 @@
 javascript: (() => {
-  // alert and exit if this program is executed out of ".salesforce.com/console"
-  if (!/\.salesforce\.com\/console/.test(location.href)) {
-    alert(`This program must be executed in ".salesforce.com/console".`);
-    return;
-  }
+    /* alert and exit if this program is executed out of "*.salesforce.com/console" */
+    if (!/\.salesforce\.com\/console/.test(location.href)) {
+        alert(`This program can be executed only in "*.salesforce.com/console".\n\ncurrent url: ${location.href}`);
+        return;
+    }
 
-  console.log(`This program is being executed in ".salesforce.com/console".`);
+    console.log(`current url: ${location.href}`);
 })();
