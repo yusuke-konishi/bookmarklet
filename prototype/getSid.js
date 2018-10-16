1af851d1ec5c56087960f2f96694f1717817615c
Yusuke Konishi <ysk.kns@gmail.com>
2018-10-16 08:30:44 +0200

    update comment/alert messages and indent for the public version

--- prototype/getSid.js
+++ prototype/getSid.js
@@ -1,12 +1,13 @@
 javascript: (() => {
-  // alert and exit if this program is executed out of ".salesforce.com/console"
-  if (!/\.salesforce\.com\/console/.test(location.href)) {
-    alert(`This program must be executed in ".salesforce.com/console".`);
-    return;
-  }
-  console.log(`current url: ${location.href}`);
+    /* alert and exit if this program is executed out of "*.salesforce.com/console" */
+    if (!/\.salesforce\.com\/console/.test(location.href)) {
+        alert(`This program can be executed only in "*.salesforce.com/console".\n\ncurrent url: ${location.href}`);
+        return;
+    }
 
-  // get sid (session id) from cookie
-  var sid = document.cookie.match(/(^|;\s*)sid=(.+?)(;|$)/)[2];
-  console.log(`sid: ${sid}`);
+    console.log(`current url: ${location.href}`);
+
+    // get sid (session id) from cookie
+    var sid = document.cookie.match(/(^|;\s*)sid=(.+?)(;|$)/)[2];
+    console.log(`sid: ${sid}`);
 })();
