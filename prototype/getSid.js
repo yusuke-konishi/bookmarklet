7a298f4fb99e29481a51089a06d77aa66dc3e5de
Yusuke Konishi <ysk.kns@gmail.com>
2018-10-21 22:17:15 +0900

    change comment style for bookmarklet

--- prototype/getSid.js
+++ prototype/getSid.js
@@ -7,7 +7,7 @@ javascript: (() => {
 
     console.log(`current url: ${location.href}`);
 
-    // get sid (session id) from cookie
+    /* get sid (session id) from cookie */
     var sid = document.cookie.match(/(^|;\s*)sid=(.+?)(;|$)/)[2];
-    console.log(`sid: ${sid}`);
+    console.log(`session id: ${sid}`);
 })();
