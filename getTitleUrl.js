ee4a4baee397cf32d23fc0822d9768a1b7fae30b
Yusuke Konishi <ysk.kns@gmail.com>
2020-05-15 22:18:57 +0900

    put target text into clipboard via temporary textarea

--- getTitleUrl.js
+++ getTitleUrl.js
@@ -1,4 +1,19 @@
 javascript: (() => {
     const titleUrl = document.title + '\n' + location.href;
-    prompt(titleUrl, titleUrl);
+
+    /* put target text into clipboard via temporary textarea */
+    let tempTextarea = document.createElement('textarea');
+    tempTextarea.textContent = titleUrl;
+
+    let bodyElement = document.getElementsByTagName('body')[0];
+    bodyElement.appendChild(tempTextarea);
+
+    tempTextarea.select();
+    document.execCommand('copy');
+    bodyElement.removeChild(tempTextarea);
+
+    alert(`The following text is now in clipboard:\n`
+          + `---\n`
+          + `${titleUrl}\n`
+          + `---`);
 })();
