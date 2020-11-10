223e4d83b04b5f91745f748c85dc727b31c2e129
Yusuke Konishi <ysk.kns@gmail.com>
2020-11-11 01:10:21 +0900

    get review list and details for 11 samples

--- downloadGooglePlayReviews.js
+++ downloadGooglePlayReviews.js
@@ -13,4 +13,31 @@ javascript: (() => {
     /* get app id information */
     const appId = matchObject[1];
     console.log(`app id: ${appId}`);
+
+    /* get review list */
+    const reviewList = document.querySelectorAll('div[jsname="fk8dgd"] > div');
+
+    /* get review details for 11 samples */
+    for (i = 0; i < 11; i++) {
+        console.log(`--- user ${i} ---`);
+
+        const userName = reviewList[i].querySelector('span[class="X43Kjb"]').innerText.trim();
+        console.log(`user name: ${userName}`);
+
+        let star = reviewList[i].querySelector('div[class="pf5lIe"] > div').getAttribute('aria-label').trim();
+        star = star.match(/(\d)\/5/)[1];
+        console.log(`star: ${star}`);
+
+        const date = reviewList[i].querySelector('span[class="p2TkOb"]').innerText.trim();
+        console.log(`date: ${date}`);
+
+        const helpful = reviewList[i].querySelector('div[class="jUL89d y92BAb"]').innerText.trim();
+        console.log(`helpful: ${helpful}`);
+
+        let review = reviewList[i].querySelector('span[jsname="fbQN7e"]').innerText.trim();
+        if (review === '') {
+            review = reviewList[i].querySelector('span[jsname="bN97Pc"]').innerText.trim();
+        }
+        console.log(`review: ${review}`);
+    }
 })();
