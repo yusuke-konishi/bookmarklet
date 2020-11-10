37a40f95dfb28a14fce7abbbf07debf48b6a9a7b
Yusuke Konishi <ysk.kns@gmail.com>
2020-11-10 23:17:17 +0900

    get app id information

--- downloadGooglePlaySurvey.js
+++ downloadGooglePlaySurvey.js
@@ -1,11 +1,16 @@
 javascript: (() => {
     /* exit if this program is executed out of play.google.com/store/apps/details */
-    const matchObject = location.href.match(/play\.google\.com\/store\/apps\/details/);
+    const url = location.href;
+    const matchObject = url.match(/play\.google\.com\/store\/apps\/details\?id=(.+?)(&|$)/);
     console.log(matchObject);
 
     if (!matchObject) {
-        alert(`This program must be executed in play.google.com/store/apps/details.\n\ncurrent url: ${location.href}`);
+        alert(`This program must be executed in play.google.com/store/apps/details.\n\ncurrent url: ${url}`);
         throw new Error('Your operation was canceled.');
     }
-    console.log(`current url: ${location.href}`);
+    console.log(`current url: ${url}`);
+
+    /* get app id information */
+    const appId = matchObject[1];
+    console.log(`app id: ${appId}`);
 })();
