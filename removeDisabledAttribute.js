f02c9efa19e2d4f6ff4e705550fd63773e229f6d
yusuke-konishi <ysk.kns@gmail.com>
2023-05-23 01:44:47 +0900

    remove "disabled" attribute

---
+++ removeDisabledAttribute.js
@@ -0,0 +1,6 @@
+javascript: (() => {
+    /* get a toggle switch to filter */
+    let switch_input = document.getElementsByClassName("switch__input")[0];
+
+    switch_input.removeAttribute("disabled");
+})();
